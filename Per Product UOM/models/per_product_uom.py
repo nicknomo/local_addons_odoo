217de7431cb72e1bf0c87f336ea119765140be58
Nick <nicknomo@gmail.com>
2016-09-10 03:35:01 -0400

    Changes to model to ensure consistency.
    Change to view for easy test input.

--- Per Product UOM/models/per_product_uom.py
+++ Per Product UOM/models/per_product_uom.py
@@ -9,15 +9,16 @@ import openerp.addons.product.product as native_product
 class local_product_uom(models.Model):
     _inherit = "product.uom"
     _name = "localproduct.uom"
-    _parent_store = True
-    _parent_name = 'category_id'
-    parent_left = fields.Integer('Parent Left',index=True)
-    parent_right = fields.Integer('Parent Right', index=True)
+    #_parent_store = True
+    #_parent_name = 'category_id'
+    #parent_left = fields.Integer('Parent Left',index=True)
+    #parent_right = fields.Integer('Parent Right', index=True)
     uom_sell = fields.Boolean('Sellable?', change_default=True)
 
-    name = fields.Char('Local Unit of Measure', required=True, translate=True)
-    category_id = fields.Many2one('productuom.class', 'Unit of Measure Category', required=True, ondelete='cascade', help="Conversion between Units of Measure can only occur if they belong to the same category. The conversion will be made based on the ratios.")
+    name = fields.Char('Unit of Measure', required=True, translate=True)
+    category_id = fields.Many2one('productuom.class', 'Unit of Measure Conversion Class', required=True, ondelete='cascade', help="Conversion between Units of Measure can only occur if they belong to the same category. The conversion will be made based on the ratios.")
 
+    _sql_constraints = [('factor_gt_zero', 'CHECK (factor!=0)', 'The conversion ratio for a unit of measure cannot be 0!'), ('uom_uniq','UNIQUE (name,category_id)','Only one entry for that UOM per category')    ]
 
     def name_create(self, cr, uid, name, context=None):
         """ The UoM category and factor are required, so we'll have to add temporary values
@@ -27,7 +28,7 @@ class local_product_uom(models.Model):
         uom_categ = self.pool.get('productuom.class')
         values = {self._rec_name: name, 'factor': 1}
         # look for the category based on the english name, i.e. no context on purpose!
-        # New BETA
+        # TODO: should find a way to have it translated but not created until actually used
         if not context.get('default_category_id'):
             categ_misc = 'Unsorted/Imported Units'
             categ_id = uom_categ.search(cr, uid, [('name', '=', categ_misc)])
@@ -43,8 +44,8 @@ class local_product_uom(models.Model):
 class product_uom_class(models.Model):
     _name = 'productuom.class'
     name = fields.Char('Name', required=True, translate=True)
-    child_ids = fields.One2many('localproduct.uom','category_id','Child Tags')
-    localuom = fields.One2many('localproduct.uom', 'id', 'Per Product Unit of Measure', required=True, help="Unit of Measure used for this products stock operation.")
+    #child_ids = fields.One2many('localproduct.uom','category_id','Marker')
+    localuom = fields.One2many('localproduct.uom', 'category_id', 'Per Product Unit of Measure', required=False, help="Unit of Measure used for this products stock operation.")
 
     _sql_constraints = [('name_uniq','UNIQUE (name)','Product UOM Conversion Class must be unique.')]
 
