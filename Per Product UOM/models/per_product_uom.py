72b4261337c31f7a931a5189417fb176d972278f
Nick <nicknomo@gmail.com>
2016-09-20 03:28:31 -0400

    Commenting

--- Per Product UOM/models/per_product_uom.py
+++ Per Product UOM/models/per_product_uom.py
@@ -160,7 +160,8 @@ class ProductTemplate(models.Model):
 
 
 
-
+# For some reason, odoo's views had trouble finding the category of the uom in sale.order.line on the fly
+# This keeps the record in the DB, and allows me to filter UOM categories.
 class NewSaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     #These computed fields are for calculating the domain on a form edit
