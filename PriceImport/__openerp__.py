7ad15fe1d46f75ea698c92c9b00e667f1f561a26
Nick <nicknomo@gmail.com>
2017-01-06 15:47:04 -0500

    Release Version 10.1.0

--- PriceImport/__openerp__.py
+++ PriceImport/__openerp__.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Per product PRICE Dancik IMPORT',
+    'summary': 'Each product can be assigned its own UoM class',
+    'category': 'Warehouse',
+    'version': '10.1.0',
+    'author': 'Nick Ross (CTC corp) - nick (at) theserverexpert (dot) com',
+    'website': 'https://github.com/nicknomo/local_addons_odoo',
+    'license': 'Creative Commons: Attribution-NonCommercial-ShareAlike 4.0 International',
+    # Clarification of NonCommercial: Commercial entities may use this module in Odoo for generating profit,
+    # as long as they do not redistribute it.  It cannot be resold, or repackaged in any way.  It cannot be
+    # provided as part of a software as a service, nor can it be included in any publicly distributed Odoo builds.
+    # If you wish to include this module in an Odoo package, distribution or SaaS deployment, please contact me first.
+    # Depending on the usage, I may license it cheaply or for no cost.
+    # All entities using their own instance of Odoo, in the course of their business, are free to use this product
     'depends': ['product','sale','purchase','stock','Per Product Price Lists'],
     'data': [],
 }
--- Sales Per UOM/models/sales.py
+++ Sales Per UOM/models/sales.py
@@ -11,18 +11,21 @@ class NewSaleOrder(models.Model):
     quoteproductuom = fields.Many2one('product.uom','Price Uom:',ondelete='cascade')
     quoteqty = fields.Integer('Pricing Qty:', default=1)
 
-    quoteuomprice = fields.Char('List:',compute='_computeuomprice')
-    quoteuomcost = fields.Char('Cost:', compute='_computeuomcost')
+    quoteuomprice = fields.Char('List Price:',compute='_computeuomprice')
+    quoteuomcost = fields.Char('Cost Price:', compute='_computeuomcost')
 
     quoteuserprice = fields.Float('Sale Price:')
-    quoteexactuomqty = fields.Char('Est Qty:',compute='_computeexactsaleqty')
+    quoteexactuomqty = fields.Char('Est. Sale Qty:',compute='_computeexactsaleqty')
 
     quotesaleqty = fields.Integer('Sale Qty:')
     quotesaleuom = fields.Char('Sale UoM', compute='_computesaleuom')
+    quotesaleuomcat = fields.Integer('Cat ID',default=1)
     #quotesaleuom = fields.Many2one(related='quoteproduct.uom_id',ondelete='cascade')
     quotecost = fields.Float('Line Price', compute='_computecost')
 
     quoteactualprice = fields.Float('sell at this price:', compute='_computeactualprice')
+    quoteblank = fields.Char(' ', readonly="True")
+    quotepriceuom = fields.Char('Sell @ PRiCE UM:', compute='_computepriceuom')
 
 
     # The default onchange returns a domain, but it also does other stuff. Since we set a default domain, we want don't want it to return a domain
@@ -31,27 +34,38 @@ class NewSaleOrder(models.Model):
     @api.multi
     @api.onchange('quoteproduct')
     def quoteproduct_id_change(self):
+
+
+        if not self.quoteproduct:
+            self.quoteqty = False
+            self.quoteproductuom = False
+            self.quoteuserprice = False
+            self.quotesaleqty = False
+            self.quotesaleuomcat = 1
+            return {}
+
         product = self.quoteproduct
 
         if (product.uom_id):
             vals = {}
             self.quoteproductuom = product.uom_id
-            domain = {'quoteproductuom': [('category_id', '=', self.quoteproduct.uom_id.category_id.id)]}
+            self.quotesaleuomcat = self.quoteproduct.uom_id.category_id.id
+            domain = {'quoteproductuom': [('category_id', '=', 1925)]}
             vals['quoteproductuom'] = self.quoteproduct.uom_id
             self.update(vals)
             return {'domain': domain}
-
-        else:
-            self.quoteqty = False
-            self.quoteproductuom = False
-            self.quoteuserprice = False
-            self.quotesaleqty = False
 
         return {}
 
     @api.depends('quoteproduct')
     def _computesaleuom(self):
         self.quotesaleuom = self.quoteproduct.uom_id.name
+        return {}
+
+    @api.depends('quoteproductuom')
+    def _computepriceuom(self):
+        if not self.quoteproductuom: return {}
+        self.quotepriceuom = '' + self.quoteproductuom.name + ''
         return {}
 
     @api.depends('quoteproduct','quoteproductuom')
@@ -78,7 +92,7 @@ class NewSaleOrder(models.Model):
             convfactor = float(float(salefactor) / float(pricefactor))
             convprice = float(float(price) * float(convfactor))
             convprice = round(convprice, 2)
-            self.quoteuomprice= '' + str(convprice) + ' per  ' + newuom.name
+            self.quoteuomprice= '' + str(convprice) + ' / ' + newuom.name
             return {}
 
         self.quoteuomprice = 'N/A'
@@ -108,7 +122,7 @@ class NewSaleOrder(models.Model):
             convfactor = float(float(salefactor) / float(pricefactor))
             convprice = float(float(cost) * float(convfactor))
             convprice = round(convprice, 2)
-            self.quoteuomcost= '' + str(convprice) + ' per  ' + newuom.name
+            self.quoteuomcost= '' + str(convprice) + ' / ' + newuom.name
             return {}
 
         self.quoteuomcost = 'N/A'
@@ -179,12 +193,68 @@ class NewSaleOrder(models.Model):
             if ( (self.quoteproduct.list_price > 0) and (convprice > 0) and (qty >= 1)  ):
                 newfactor = float(self.quoteuserprice / convprice)
                 realprice = float (self.quoteproduct.list_price * newfactor * qty)
+                realprice = round(realprice, 2)
                 self.quotecost = realprice
 
 
     @api.depends('quoteproduct', 'quoteproductuom', 'quotesaleqty', 'quotesaleuom', 'quoteuserprice')
     def _computeactualprice(self):
-        self.quoteactualprice = 1
+        qty = self.quotesaleqty
+        price = self.quoteproduct.list_price
+        uom = self.quoteproduct.uom_id
+        newuom = self.quoteproductuom
+
+        if uom and newuom and price:
+            if (uom.uom_type == 'bigger'):
+                salefactor = uom.factor_inv
+            else:
+                salefactor = uom.factor
+
+            if (newuom.uom_type == 'bigger'):
+                pricefactor = newuom.factor_inv
+            else:
+                pricefactor = newuom.factor
+
+            if (pricefactor == 0):
+                self.quoteuomprice = 'Div By 0'
+                return {}
+
+            if (pricefactor == 0):
+                self.quoteuomprice = 'Div By 0'
+                return {}
+
+            convfactor = float(float(salefactor) / float(pricefactor))
+            convprice = float(float(price) * float(convfactor))
+            convprice = round(convprice, 2)
+
+
+            if ((self.quoteproduct.list_price > 0) and (convprice > 0) and (qty >= 1)):
+                newfactor = float(self.quoteuserprice / convprice)
+                realprice = float(self.quoteproduct.list_price * newfactor * 1)
+                realprice = round(realprice, 2)
+                self.quoteactualprice = realprice
+
+
+    @api.multi
+    def newlinecreate(self):
+
+        if not (self.quoteproduct and self.quotesaleqty and self.quoteproductuom and self.quoteactualprice):
+            return
+
+        if not (self.quoteproduct.description):
+            description = self.quoteproduct.name
+        else:
+            description = self.quoteproduct.description
+
+
+        vals = {'order_id': self.id, 'name': description, 'price_unit': self.quoteactualprice, 'product_id': self.quoteproduct.id, 'product_uom_qty': self.quotesaleqty, 'product_uom': self.quoteproduct.uom_id.id}
+        newline = self.env['sale.order.line'].create(vals)
+        newline.product_id_change()
+        tempvals = { 'price_unit': self.quoteactualprice, }
+        newline.write(tempvals)
+        self.quoteproduct = False
+
+
 
 
 
